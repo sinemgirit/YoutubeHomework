fb080637407566ec7c0d777da555763d2a3aa063
Sinem Girit <sinemdursun106@gmail.com>
2024-03-15 11:46:51 +0300

    test

--- Person.cs
+++ Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace YoutubeHomework
 {
-	class Person : Customer
+	class Person : Customer 
 	{
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
